ca85a5b0a9d7e105a4256cfe02162f19643ffc43
Karolina <dyrda.karolina@gmail.com>
2023-06-05 14:33:12 +0200

    test(007): Transactions test cases

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   retries: {
     runMode: 2,
-    openMode: 0,
+    openMode: 1,
   },
   e2e: {
     setupNodeEvents(on, config) {
